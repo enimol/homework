2162c4a2ebbe46a19a9ab3fcf114303b81f08bb2
Imre Molnar <en.molnar.imre@gmail.com>
2019-04-08 00:04:14 +0200

    no input file, arg[0] is the count, randomized csv output

--- src/main/java/com/howo/HoWoMapper.java
+++ src/main/java/com/howo/HoWoMapper.java
@@ -7,16 +7,29 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+
+import com.github.javafaker.Faker;
  
 public class HoWoMapper extends Mapper<LongWritable, Text, Text, Text>{
  
 	private final static IntWritable one = new IntWritable(1);
-    private Text word = new Text();
+    private Integer count = 0;
+    public static final String COUNT = "app.config.count";
+    Faker faker = new Faker();
+    
+    @Override
+    protected void setup(Context context) {
+       this.count = Integer.parseInt(context.getConfiguration().get(COUNT));
+    }
 
     @Override
 	protected void map(LongWritable key, Text value,
 			Context context)
 			throws IOException, InterruptedException {
-		context.write(new Text("bubu") , new Text("fifi") );
+    	 for(int i=0; i < this.count; i++) {
+				String lastName = faker.name().fullName();
+				String firstName = faker.name().firstName();
+    			context.write(new Text(lastName) , new Text(firstName) );
+    	 }
 	}
 }
--- src/main/java/com/howo/HomeWork.java
+++ src/main/java/com/howo/HomeWork.java
@@ -1,6 +1,8 @@
 package com.howo;
 
 import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -12,6 +14,8 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
 
 public class HomeWork extends Configured implements Tool{
 	
@@ -22,17 +26,32 @@ public class HomeWork extends Configured implements Tool{
  
 	public int run(String[] args) throws Exception {
 		if (args.length != 2) {
-			System.err.printf("Usage: %s [generic options] <input> <output>\n",
+			System.err.printf("Usage: %s [generic options] <count> <output>\n",
 					getClass().getSimpleName());
 			ToolRunner.printGenericCommandUsage(System.err);
 			return -1;
 		}
 	
 		Job job = new org.apache.hadoop.mapreduce.Job();
+
+		Configuration configuration = job.getConfiguration();
+		configuration.set("mapred.textoutputformat.separatorText", ",");
+		configuration.set("mapred.textoutputformat.separator", ",");
+		configuration.set("fs.defaultFS","hdfs://sandbox-hdp.hortonworks.com:8020");
+		configuration.set(HoWoMapper.COUNT, args[0]);
+
+		FileSystem fs = FileSystem.get(configuration);
+		FSDataOutputStream stream = fs.create(new Path("/tmp/tmp"));
+		stream.write(args[0].getBytes());
+		stream.flush();
+		stream.close();
+
+		FileInputFormat.addInputPath(job, new Path("/tmp/tmp"));
+
 		job.setJarByClass(HomeWork.class);
 		job.setJobName("HomeWork");
-		
-		FileInputFormat.addInputPath(job, new Path(args[0]));
+
+//		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 	
 		job.setOutputKeyClass(Text.class);
